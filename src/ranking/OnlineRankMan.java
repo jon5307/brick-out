84f884c65c7b80bea36b1e94d3341e74c2463e44
Sohal3626 <89302184+Sohal3626@users.noreply.github.com>
2022-12-09 12:32:11 +0900

    OnlineRank Man클래스의 onlineRegRank 매소드 작성중

--- src/ranking/OnlineRankMan.java
+++ src/ranking/OnlineRankMan.java
@@ -4,6 +4,7 @@ import java.sql.*;
 
 public class OnlineRankMan {
     private Connection conn;
+    private Statement state = null;
 
     public OnlineRankMan(){
         OnlineLeaderBoard lb = new OnlineLeaderBoard();
@@ -11,6 +12,11 @@ public class OnlineRankMan {
 
     public void onlineRegRank(String name, int score){
         int rank = 0;
-
+        try {
+            state.executeUpdate(" INSERT INTO Brick (Ranking, Name, Score) " +
+                    "VALUE ("+rank+","+name+","+score+") ");
+        } catch (SQLException e) {
+            System.out.println("insert err");
+        }
     }
 }
